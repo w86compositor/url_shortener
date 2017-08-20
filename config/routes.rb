a05df849ba795ac72685ed7f1c737ced20677909
Waldo <w86ccompositor@gmail.com>
2017-08-20 17:24:10 -0400

    Modified routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
+	root to: 'urls#new'
   resources :urls, only [:new, :create]
-
-  root to: 'urls#new'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
