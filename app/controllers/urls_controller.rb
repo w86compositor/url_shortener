0623dd810c597f5a6e816466c3244e8d6d1a95e2
Waldo <w86ccompositor@gmail.com>
2017-08-20 22:14:37 -0400

    Done

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -4,8 +4,9 @@ class UrlsController < ApplicationController
 
   def create
   	if !params[:url].blank?
-      client = Bitly.client
-      @url = client.shorten(params[:url])
+  		redirect_to root_path, notice: "Please enter a valid url"
+ 	else
+ 		@url = Bitly.client.shorten(params[:url])
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :urls, only [:new, :create]
+  resources :urls
 
   root to: 'urls#new'
 
